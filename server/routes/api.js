715089926d94dc3d2f94de4bebe5f9da4168fe87
aliciapflaumer <alpfla7@gmail.com>
2018-05-10 19:34:23 -0400

    Added mean-auth npm package
    - Used `npm install mean-auth`

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,8 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 // declare axios for making http requests
-const axios = require('axios');
-const API = 'https://jsonplaceholder.typicode.com';
+// const axios = require('axios');
+// const API = 'https://jsonplaceholder.typicode.com';
+
+// const NewsAPI = require('newsapi');
+// const newsapi = new NewsAPI('825de09aa7b84ba2895d7d1ae9c1f1cd');
+//
+// // To query sources
+// // All options are optional
+// newsapi.v2.sources({
+//   category: 'technology',
+//   language: 'en',
+//   country: 'us'
+// }).then(response => {
+//   console.log(response);
+//   /*
+//     {
+//       status: "ok",
+//       sources: [...]
+//     }
+//   */
+// });
 
 /* GET api listing. */
 router.get('/', (req, res) => {
@@ -11,16 +30,16 @@ router.get('/', (req, res) => {
 });
 
 // Get all posts
-router.get('/posts', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
-});
-
+// router.get('/posts', (req, res) => {
+//   // Get posts from the mock api
+//   // This should ideally be replaced with a service that connects to MongoDB
+//   axios.get(`${API}/posts`)
+//     .then(posts => {
+//       res.status(200).json(posts.data);
+//     })
+//     .catch(error => {
+//       res.status(500).send(error)
+//     });
+// });
+//
 module.exports = router;
